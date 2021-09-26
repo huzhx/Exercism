91b73f57d7073ebbef6519125e6dfdcd20feb050
Zhaoxian Hu <huzhx@uber.com>
2021-09-25 19:41:52 -0700

    refactor by using errors and more concise loop

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,13 +1,15 @@
 package hamming
 
-import "fmt"
+import (
+	"errors"
+)
 
 func Distance(a, b string) (int, error) {
 	hammingDist := 0
 	if len(a) != len(b) {
-		return -1, fmt.Errorf("a and b have different lengths")
+		return -1, errors.New("a and b have different lengths")
 	}
-	for i := 0; i < len(a); i++ {
+	for i := range a {
 		if a[i] != b[i] {
 			hammingDist++
 		}
