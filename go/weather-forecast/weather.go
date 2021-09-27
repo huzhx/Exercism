359f3c3c2483a51cfb31cddd5baa193c38e86c8d
Zhaoxian Hu <huzhx@uber.com>
2021-09-26 17:42:22 -0700

    refactor by providing more meaningful comments

--- go/weather-forecast/weather.go
+++ go/weather-forecast/weather.go
@@ -1,13 +1,13 @@
-// Package...
+// Package weather provides ability to check weather information
 package weather
 
-// CurrentCondition...
+// CurrentCondition stores current weather condition
 var CurrentCondition string
 
-// CurrentLocation...
+// CurrentLocation stores current location information
 var CurrentLocation string
 
-// Forecast ...
+// Forecast returns a string that shows the current weather condition at the current location
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
