cb5fc6a7d93157249405bea66cd7728b0f3cb3e6
Zhaoxian Hu <huzhx@uber.com>
2022-06-26 00:33:12 -0700

    added bird-watcher

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,36 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	count := 0
+	for _, birds := range birdsPerDay {
+		count += birds
+	}
+	return count
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	min := (week - 1) * 7
+	max := min + 6
+	count := 0
+	for i := min; i <= max; i++ {
+		if i < len(birdsPerDay) {
+			count += birdsPerDay[i]
+		}
+	}
+	return count
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := range birdsPerDay {
+		if i%2 == 0 {
+			birdsPerDay[i]++
+		}
+	}
+	return birdsPerDay
+}
